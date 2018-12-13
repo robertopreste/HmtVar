25eae8fd3458acb40db7cfd7f06c374bd82c2996
Roberto Preste <roberto.preste@gmail.com>
2018-12-13 11:44:17 +0100

    Add basic app structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 # Created by Roberto Preste
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+app.config.from_object("config")
+Bootstrap(app)
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
