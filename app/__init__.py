0d42e7e96d73b30cce9f62f3024cd883ba1cc852
Roberto Preste <roberto.preste@gmail.com>
2018-12-13 16:01:29 +0100

    Add final HTML files

--- app/__init__.py
+++ app/__init__.py
@@ -14,3 +14,21 @@ Bootstrap(app)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+from .site.views import www
+from .api.views import res
+
+from .api.views import api
+from .api.endpoints.mains import ns as main_namespace
+from .api.endpoints.variabs import ns as variab_namespace
+from .api.endpoints.annots import ns as annot_namespace
+from .api.endpoints.plasmy import ns as plasmy_namespace
+from .api.endpoints.predicts import ns as predict_namespace
+api.add_namespace(main_namespace)
+api.add_namespace(variab_namespace)
+api.add_namespace(annot_namespace)
+api.add_namespace(plasmy_namespace)
+api.add_namespace(predict_namespace)
+
+app.register_blueprint(www)
+app.register_blueprint(res, url_prefix="/api")
+
