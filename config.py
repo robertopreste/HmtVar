82acf02d011fd0d51785f6cbaf546a90933f47af
Roberto Preste <roberto.preste@gmail.com>
2018-12-13 16:01:54 +0100

    Complete config and db manipulation scripts

---
+++ config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Created by Roberto Preste
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = "test"  # change this!
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+EXPLAIN_TEMPLATE_LOADING = False
+
+# database
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "hmtvar.db")
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, "db_repo")
+
